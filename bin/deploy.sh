3d4c7a0234a4c29be113403ea5f028e6b7cc85e8
Guillaume Ponçon <gponcon@gmail.com>
2024-12-17 15:58:24 -0300

    Relecture

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,12 @@
 
 APP_ROOT="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 
-cd $APP_ROOT && npm run build
+cd $APP_ROOT
+npm run build
+rsync -rv --delete --exclude README.md dist/* spmsn.github.io/
 LAST_MESG=`git log -1 --pretty=%B | head -n 1`
-cd dist && git pull --rebase && git add . && git commit -m "$LAST_MESG" && git push -u origin main
+cd spmsn.github.io && \
+    git pull && \
+    git add . && \
+    git commit -m "$LAST_MESG" && \
+    git push -u origin main
